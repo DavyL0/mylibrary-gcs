b4837234124e6261acf1bf0ff005a6aacfbf2dff
Davy Lopes <d4vyl0pes@gmail.com>
2026-05-23 00:30:18 -0300

    fix (frontend): correção dos componentes (#17)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './page/dashboard/dashboard';
-import { LivroFilter } from './page/livro-filter/livro-filter';
 import { EmprestimosAtrasados } from './page/emprestimos-atrasados/emprestimos-atrasados';
 import {CategoriaForm} from './page/categoria-form/categoria-form';
 
-type Pagina = 'dashboard' | 'categoria' | 'livros' | 'atrasados';
+import { CategoriaList } from './page/categoria-list/categoria-list';
+import { LivroList } from './page/livro-list/livro-list';
+import { LivroFormComponent } from './page/livro-form/livro-form';
+import { EmprestimoList } from './page/emprestimo-list/emprestimo-list';
+
+type Pagina = 'dashboard' | 'categoria-form' | 'categoria-list' | 'livro-form' | 'livros' | 'atrasados' | 'emprestimos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, DashboardComponent, LivroFilter, EmprestimosAtrasados, CategoriaForm],
+  imports: [CommonModule, DashboardComponent, LivroList, EmprestimosAtrasados, CategoriaForm, CategoriaList, LivroFormComponent, EmprestimoList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
